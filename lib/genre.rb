8a074d3cc0a719d20f91f46bbbfb89c8a2e4bc18
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-29 20:00:20 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,2 +1,16 @@
 class Genre
+  attr_reader :name
+
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artists
+  end
 end
