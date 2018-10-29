7a99041a2c8f188de7a3e7f8650e1b27f33d64db
Leifur Sigurdsson <leifurerik@gmail.com>
2018-10-29 19:45:24 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,4 +10,14 @@ class Artist
   def self.all
     @@all
   end
+
+  def new_song(name, genre)
+    Song.new(self, genre)
+  end
+
+  def songs
+    Song.all.select do |song|
+      song.artist == self
+    end
+  end
 end
